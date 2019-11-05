2f117ffc94ac00b39d0c98bb4e1d00c034196194
MalcolmScruggs <mookie.dev@gmail.com>
2019-11-05 09:23:56 -0500

    Improve navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {action, decorate, observable} from "mobx";
+
 import FoodToxicity from "./pages/FoodToxicity";
 import VetRecords from "./pages/VetRecords";
 import Home from "./pages/Home";
 import PetStore from "./stores/Pets"
-import {action, decorate, observable} from "mobx";
+import Navigation from "./Navigation";
 
 decorate(PetStore, {
     pets: observable,
@@ -17,34 +19,23 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <div>
-                    <nav>
-                        <ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/food_toxicity">Food Toxicity</Link>
-                            </li>
-                            <li>
-                                <Link to="/vet_records">Vet Records</Link>
-                            </li>
-                        </ul>
-                    </nav>
-
-                    {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-                    <Switch>
-                        <Route path="/food_toxicity">
-                            <FoodToxicity petStore={petStore}/>
-                        </Route>
-                        <Route path="/vet_records">
-                            <VetRecords petStore={petStore}/>
-                        </Route>
-                        <Route path="/">
-                            <Home petStore={petStore}/>
-                        </Route>
-                    </Switch>
+                <div className="container-fluid">
+                    <div className="row">
+                        <main className="main-content col-lg-12 col-md-12 col-sm-12 p-0">
+                            <Navigation/>
+                            <Switch>
+                                <Route path="/food_toxicity">
+                                    <FoodToxicity petStore={petStore}/>
+                                </Route>
+                                <Route path="/vet_records">
+                                    <VetRecords petStore={petStore}/>
+                                </Route>
+                                <Route path="/">
+                                    <Home petStore={petStore}/>
+                                </Route>
+                            </Switch>
+                        </main>
+                    </div>
                 </div>
             </Router>
         );

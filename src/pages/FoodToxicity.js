2a89fc985926940507f3db93908589435547e205
Anubhav Sharma <anubhav@erponweb.com>
2019-11-07 20:11:46 -0500

    got toxicity scanner working

--- src/pages/FoodToxicity.js
+++ src/pages/FoodToxicity.js
@@ -1,16 +1,118 @@
-import React from "react";
+import React, { Component } from "react";
+import {
+    Card,
+    CardBody,
+    CardSubtitle,
+    CardTitle,
+    Col,
+    Form,
+    FormGroup,
+    FormFeedback,
+    Label,
+    Input,
+} from 'reactstrap';
 import {observer} from "mobx-react";
 
-function FoodToxicity({ petStore, foodStore }) {
-    const { pets } = petStore
-    const { foods} = foodStore
-    return (
-        <div>
-            <h1>Food Toxicity</h1>{
-            foods.map((food,idx) => <div>{food.name}&nbsp;&nbsp;{food.type}</div>)}
-        </div>
-    );
+class FoodToxicity extends Component {
 
+    constructor(props) {
+        super();
+        const {foodStore} = props;
+        const {foods} = foodStore;
+        this.state = {
+            selected_type: 'dog',
+            current_foods: (foods.filter(food => food.type === 'dog')).map(food => food.name),
+            selected_food: '',
+        }
+    }
+
+    selectAnimalType = (e) => {
+        e.preventDefault();
+        const {foodStore} = this.props;
+        const {foods} = foodStore;
+        let food;
+        let foodList = [];
+        for (food of foods) {
+            // console.log(food.name+"|"+food.type);
+            if (food.type === this.state.selected_type.toLowerCase()) {
+                foodList.push(food.name);
+            }
+        }
+        this.setState({
+            selected_type: e.target.value.toLowerCase(),
+            current_foods: foodList,
+        });
+        console.log("State: ", this.state);
+    };
+
+    checkToxicity = (e) => {
+        e.preventDefault();
+        this.setState({
+            selected_food: e.target.value.toLowerCase(),
+        })
+    }
+
+    render() {
+        let checkFood;
+        if (this.state.selected_food === '') {
+            checkFood = 
+            <FormGroup>
+                <Label for="selectedFood">Food:</Label>
+                <Input type="text" name="selectedFood" id="selectedFood" placeholder="Search food" required 
+                    onChange={this.checkToxicity}>
+                </Input>
+            </FormGroup>;
+        } else {
+            console.log(this.state.selected_food);
+            console.log(this.state.current_foods.indexOf(this.state.selected_food) >= 0);
+            if (this.state.current_foods.indexOf(this.state.selected_food) >= 0) {
+                checkFood = 
+                <FormGroup>
+                                <Label for="selectedFood">Food:</Label>
+                                <Input invalid type="text" name="selectedFood" id="selectedFood" placeholder="Search food" required 
+                                    onChange={this.checkToxicity}>
+                                </Input>
+                                <FormFeedback>Toxic</FormFeedback>
+                </FormGroup>;
+            } else {
+                checkFood = 
+                <FormGroup>
+                                <Label for="selectedFood">Food:</Label>
+                                <Input valid type="text" name="selectedFood" id="selectedFood" placeholder="Search food" required 
+                                    onChange={this.checkToxicity}>
+                                </Input>
+                                <FormFeedback valid>Safe</FormFeedback>
+                </FormGroup>;
+            }
+        }
+        return (
+            <div className="container">
+                <Col md="12" lg="6" className="my-3">
+                <Card>
+                    <CardBody>
+                        <CardTitle className="h3 font-weight-bolder">Food Toxicity</CardTitle>
+                        <CardSubtitle>Lookup toxicity for specific foods before feeding your pet!</CardSubtitle>
+                    </CardBody>
+                </Card>
+                <Card>
+                    <CardBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="petTypeFood">Pet Type:</Label>
+                                <Input type="select" name="petTypeFood" id="petTypeFood" onChange={this.selectAnimalType}>
+                                        <option>Dog</option>
+                                        <option>Cat</option>
+                                </Input>
+                            </FormGroup>
+                            {checkFood}
+                        </Form>
+                    </CardBody>
+                </Card>
+
+            </Col>
+            </div>
+        );
+    }
 }
 
 export default observer(FoodToxicity)
